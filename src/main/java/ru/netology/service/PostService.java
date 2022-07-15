bc83e3c30224129e44be7f5cefbd6265e2bcf0f7
Kirill Zubkov <kirill131313@gmail.com>
2022-07-15 14:55:10 +0300

    jspr_05_di_java

--- src/main/java/ru/netology/service/PostService.java
+++ src/main/java/ru/netology/service/PostService.java
@@ -6,7 +6,6 @@ import ru.netology.model.Post;
 import ru.netology.repository.PostRepository;
 
 import java.util.Collection;
-@Service
 public class PostService {
   private final PostRepository repository;
 
